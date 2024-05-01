30e04d5237bf2d1b14112187f41f8e8aef4d1f5d
Sagar Dhone <sdhone98@gmail.com>
2024-05-01 23:59:50 +0530

    fix

--- src/componets/Experiance/ProjectCard.jsx
+++ src/componets/Experiance/ProjectCard.jsx
@@ -9,12 +9,11 @@ const ProjectCard = ({
   correntMode,
 }) => {
 
-  const imagePath = `/src/resources/${imageName}`;
   return (
     <div className="project-info" style={{flexDirection: `${id/2 == 0? "row":"row-reverse"}`,
     border: `1px solid ${correntMode? "rgba(245,245,245,.3)" : "rgba(0,0,0,.3)"}`}}>
       <div className="img">
-        <img src={imagePath}/>
+        <img src={`/project_images/background/${imageName}`} alt={imageName}/>
       </div>
       <div className="center-line" style={{backgroundColor: `${correntMode? "rgba(245,245,245,.3)":"rgba(0,0,0,.3)"}`}}></div>
       <div className="card">
