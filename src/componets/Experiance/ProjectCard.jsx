51d744e89c3a2ebdcf81e41b091da00e6b8270da
Sagar Dhone <sdhone98@gmail.com>
2024-08-03 01:31:57 +0530

    sampravah project added - sagar

--- src/componets/Experiance/ProjectCard.jsx
+++ src/componets/Experiance/ProjectCard.jsx
@@ -10,7 +10,7 @@ const ProjectCard = ({
 }) => {
 
   return (
-    <div className="project-info" style={{flexDirection: `${id/2 == 0? "row":"row-reverse"}`,
+    <div className="project-info" style={{flexDirection: `${id%2 == 0? "row":"row-reverse"}`,
     border: `1px solid ${correntMode? "rgba(245,245,245,.3)" : "rgba(0,0,0,.3)"}`}}>
       <div className="img">
         <img src={`/project_images/background/${imageName}`} alt={imageName}/>
